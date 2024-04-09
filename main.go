cd30bddf16d0a502b48859c9c2b8dc1745573052
catalinfl <catalinfl02@gmail.com>
2024-04-09 23:59:51 +0300

    .

--- main.go
+++ main.go
@@ -2,12 +2,42 @@ package main
 
 import (
 	"fmt"
-
-	dynamicprogramming "github.com/catalinfl/algorithms/algorithms/dynamic-programming"
+	"sort"
 )
 
 func main() {
 
-	fmt.Println(dynamicprogramming.Sum(100))
+	var prices []int = []int{7, 1, 5, 3, 6, 4}
+	fmt.Println(maxProfit(prices))
+
+}
+
+func maxProfit(prices []int) int {
+	l := len(prices)
+
+	if l == 0 {
+		return 0
+	}
+
+	b := make([]int, len(prices))
+
+	copy(b, prices)
+
+	sort.Ints(b)
+
+	minPrice := b[0]
+
+	for i := 0; i < l; i++ {
+		b[i] = 0
+		fmt.Println(b[i])
+	}
+
+	for i := 0; i < l; i++ {
+		b[i] = prices[i] - minPrice
+	}
+
+	sort.Ints(b)
+
+	return b[len(b)-1]
 
 }
